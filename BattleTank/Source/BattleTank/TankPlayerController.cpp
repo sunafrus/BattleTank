a0edfbbba84e6f626e15edac6b5df5c18cd67ab3
DESKTOP-LJOS1IK\hugom <hugo.matthews@googlemail.com>
2017-06-15 20:51:07 +0100

    Using DeprojectScreenToWorld

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,12 +3,11 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
-
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("TankPlayerController - Begin Play"));
+	PrimaryActorTick.bCanEverTick = true;
 
 	ATank* ControlledTank = GetControlledTank();
 	if (!ControlledTank)
@@ -22,7 +21,60 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank())
+	{
+		return;
+	}
+
+	FVector HitLocation; /// Out parameter
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		/// Tell the controlled tank to aim to hit this point
+	}
+}
+
+// Get world location of the linetrace hit through the crosshair, return true if it hits, false if not
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	/// Find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	FVector2D ScreenLocation = FVector2D(ViewportSizeX * CrossHairLocationX, ViewportSizeY * CrossHairLocationY);
+
+	/// Find the look direction through the crosshair
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Look Direction: %s"), *LookDirection.ToString());
+	}
+	
+	/// Line-trace along that LookDirection, and see what we hit (up to max range)
+	return true;
+}
+
+// "De-project" the screen position of the crosshair to a world direction
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector CameraWorldLocation; /// To be discarded
+	return DeprojectScreenPositionToWorld(ScreenLocation.X,
+		ScreenLocation.Y,
+		CameraWorldLocation,
+		LookDirection
+	);
+
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -17,6 +17,21 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 private:
 	ATank* GetControlledTank() const;
 	
-	void BeginPlay() override;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	// Make the tank aim so that a shot would hit at the crosshair
+	void AimTowardsCrosshair();
+
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairLocationX = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairLocationY = 0.33333f;
 
 };
