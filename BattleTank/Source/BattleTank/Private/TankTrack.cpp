ab4f2d0a368940d624bc9d6a44396f1973f66884
DESKTOP-LJOS1IK\hugom <hugo.matthews@googlemail.com>
2017-06-22 17:58:55 +0100

    Fly-by-Wire Control System

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle)
+
+	// TODO: Clamp actual throttle value
+
+	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
+
+
---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTrack is used to apply forces to a tank.
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	// Max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000.0f;
+	
+};
